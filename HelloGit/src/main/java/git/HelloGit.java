c7c790bb8e8686e50332c4420a1e0c85f950976e
lidongxin112 <72932739+lidongxin112@users.noreply.github.com>
2020-12-27 16:49:38 +0800

    添加文件

--- HelloGit/src/main/java/git/HelloGit.java
+++ HelloGit/src/main/java/git/HelloGit.java
@@ -6,7 +6,5 @@ public class HelloGit {
         System.out.println("更新1");
         System.out.println("更新3");
         System.out.println("aa");
-        System.out.println("创建分支");
-        System.out.println("主干添加");
     }
 }
